bbda870830228161f3127a22d3bb05a27014ee2f
Holger Grosse-Plankermann <holgergp@gmail.com>
2017-05-04 18:41:34 +0200

    Added config for fishshell

---
+++ .config/fish/config.fish
@@ -0,0 +1,23 @@
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+## SDKs ##
+set -x JAVA_7_HOME (/usr/libexec/java_home -v 1.7)
+set -x JAVA_8_HOME (/usr/libexec/java_home -v 1.8)
+set -x JAVA_HOME $JAVA_8_HOME
+
+
+set PATH /usr/local/bin /usr/local/sbin $JAVA_HOME/bin $PATH
+
+## rbenv
+#set PATH $HOME/.rbenv/bin $PATH
+#set PATH $HOME/.rbenv/shims $PATH
+#rbenv rehash >/dev/null ^&1
+
+## nvm
+set -x NVM_HOME $HOME/.nvm
+
+set -x MAVEN_OPTS "-Xmx512m -XX:MaxPermSize=128m"
+
---
+++ .config/fish/functions/__bass.py
@@ -0,0 +1,1 @@
+/Users/hgp/.config/fisherman/bass/functions/__bass.py
