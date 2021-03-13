7208773950184c1f2499fb43c9cd0b1d3946f1e1
Holger Grosse-Plankermann <holgergp@gmail.com>
2021-03-13 16:51:07 +0100

    Switch to asdf and version starship.toml

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,10 +4,10 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
 ## SDKs ##
-set -x JAVA_8_HOME (/usr/libexec/java_home -v 1.8)
-set -x JAVA_11_HOME (/usr/libexec/java_home -v 11)
+#set -x JAVA_8_HOME (/usr/libexec/java_home -v 1.8)
+#set -x JAVA_11_HOME (/usr/libexec/java_home -v 11)
 
-set -x JAVA_HOME $JAVA_11_HOME
+#set -x JAVA_HOME $JAVA_11_HOME
 
 set -x GROOVY_HOME /usr/local/opt/groovy/libexec
 
@@ -17,19 +17,15 @@ set ANDROID_HOME /Users/holgergp/Library/Android/sdk
 
 set -x N_PREFIX /Users/holgergp/.n
 
-set PATH /usr/local/bin /usr/local/sbin $JAVA_HOME/bin $ANDROID_HOME $ANDROID_HOME/emulator  $ANDROID_HOME/platform-tools $HOME/.fastlane/bin $GROOVY_HOME "$HOME/.cargo/bin" $PATH
+set PATH /usr/local/bin /usr/local/sbin $ANDROID_HOME $ANDROID_HOME/emulator  $ANDROID_HOME/platform-tools $HOME/.fastlane/bin $GROOVY_HOME "$HOME/.cargo/bin" $PATH
 
 set -x MOB_NEXT_STAY true
 
-
 if status is-interactive
     # These commands need to run when I run fish interactively
-    nvm use default --silent
+    # nvm use default --silent
     source (rbenv init -|psub)
 end
-
-#status --is-interactive; and nvm use default --silent
-#status --is-interactive; and source (rbenv init -|psub)
 
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
@@ -37,6 +33,10 @@ if not functions -q fisher
     fish -c fisher
 end
 
+source /usr/local/opt/asdf/asdf.fish
+
 starship init fish | source
 
 thefuck --alias | source
+
+
